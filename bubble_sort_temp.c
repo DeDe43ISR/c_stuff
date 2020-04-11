333ead7e853b9c8ed67a8247ee4210dadadf8282
Yarden Dahan <yarden987654321@gmail.com>
2020-04-11 14:14:22 +0300

    site way to do bubble sort
    Signed-off-by: Yarden Dahan <yarden987654321@gmail.com>

---
+++ bubble_sort_temp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ 
+void bubble_sort (int *dataBase, int arraySize) {
+    int counter, temp, limit = arraySize, s = 1;
+    while (s) {
+        s = 0;
+        for (counter = 1; counter < limit; counter++) {
+            if (dataBase[counter] < dataBase[counter - 1]) {
+                tmep = dataBase[counter];
+                dataBase[counter] = dataBase[counter - 1];
+                dataBase[counter - 1] = tmep;
+                s = 1;
+            }
+        }
+        j--;
+    }
+}
+ 
+int main () {
+    int dataBase[] = {15, 56, 12, -21, 1, 659, 3, 83, 51, 3, 135, 0};
+    int arraySize = dataBase / sizeof dataBase[0];
+    int counter;
+    for (counter = 0; counter < n; counter++)
+        printf("%d%s", dataBase[counter], counter == arraySize- 1 ? "\n" : " ");
+    bubble_sort(dataBase, n);
+    for (counter = 0; counter < n; counter++)
+        printf("%d%s", dataBase[counter], counter == arraySize- 1 ? "\n" : " ");
+    return 0;
+}
