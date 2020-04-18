d9e26ab4a646c5e76bbe4f4f6ae7638e4ef81262
Yarden Dahan <yarden987654321@gmail.com>
2020-04-18 19:50:57 +0300

    forgot to add the main file..
    Signed-off-by: Yarden Dahan <yarden987654321@gmail.com>

--- snake/snake.c
+++ snake/snake.c
@@ -2,44 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "menus.h"
+#include "game.h"
 
-int snakeLenght = 10;
-int gameOver = 0;
-int snakeYX[2][20];
-int fruitYX[2] = {9, 17};
-int playerScore = 0;
-int input = 0;
-int keepMove;
+int menuInput = 0;
 int yMax, xMax, height, width, startY, startX;
 int difficulty = 100000; //default is easy
 
-//move the snake according to the keys that are being pressed
-void moveSnake(void) {
 
-    //moving the snake location in the arrey
-    for (int i = snakeLenght; i > 0; i--) {
-        snakeYX[0][i] = snakeYX[0][i-1];
-        snakeYX[1][i] = snakeYX[1][i-1];
-    }
-
-    //saving the key only if a key was pressed
-    if (input != ERR) {
-        keepMove = input;
-    }
-
-    if (keepMove == KEY_RIGHT) {
-        snakeYX[1][0]++;
-    }
-    else if (keepMove == KEY_LEFT) {
-        snakeYX[1][0]--;
-    }
-    else if (keepMove == KEY_UP) {
-        snakeYX[0][0]--;
-    }
-    else if (keepMove == KEY_DOWN) {
-        snakeYX[0][0]++;
-    }
-}
 //setting the game window according to the terminal
 void setScreen (void) {
     getmaxyx(stdscr, yMax, xMax);
@@ -53,97 +22,6 @@ void setScreen (void) {
 
     if ((width % 2) != 0)
         width--;
-
-}
-//moves the snake to the opposite border once he hit a border
-void checkBor(void) {
-    
-    if (snakeYX[0][0] == (height - 1))
-        snakeYX[0][0] = 1;
-
-    else if (snakeYX[0][0] == 0)
-        snakeYX[0][0] = (height - 1);
-
-    else if (snakeYX[1][0] >= (width - 1))
-        snakeYX[1][0] = 1;
-
-    else if (snakeYX[1][0] == 0)
-        snakeYX[1][0] = (width - 1);
-
-}
-
-void fruitCor(void) {
-    fruitYX[0] = (rand() % (height - 4));
-    fruitYX[1] = (rand() % (width - 2));
-}
-
-void play(void) {
-
-    //snake starting point and direction
-    snakeYX[1][0] = 20;
-    snakeYX[0][0] = 8;
-    keepMove = KEY_RIGHT;
-
-    //game window initialization
-    WINDOW * game = newwin(height, width, startY, startX);
-    refresh();
-    box (game, 0, 0);
-    keypad(game, TRUE);
-    nodelay(game, TRUE);
-
-    //score window initialization
-    WINDOW * score = newwin(5, (width/2), (startY - 5), startX);
-    refresh();
-    box (score, 0, 0);
-    mvwprintw(score, 2 , 2, "Score : %d", playerScore);
-    wrefresh(score);
-
-    //prints first fruit
-    mvwaddch(game, fruitYX[0], fruitYX[1], '@');
-
-    //making sure game will start on second run
-    gameOver = 0;
-
-    while (!gameOver) {
-
-        input = wgetch(game); //getting the key that was pressed
-
-        moveSnake();
-
-        //print the snake and the fruit
-        mvwaddch(game, snakeYX[0][snakeLenght], snakeYX[1][snakeLenght], ' ');
-        mvwaddch(game, snakeYX[0][0], snakeYX[1][0], '*');
-//        mvwprintw(game, snakeYX[0][0], snakeYX[1][0], "%d", snakeYX[1][0]);
-        wrefresh(game);
-
-        //increase the snake size if a fruit was eaten
-        if (snakeYX[0][0] == fruitYX[0] && snakeYX[1][0] == fruitYX[1]) {
-            snakeLenght++;
-            playerScore++;
-            mvwaddch(game, fruitYX[0], fruitYX[1], ' ');
-            mvwprintw(score, 2, 10, "%d", playerScore);
-            fruitCor();
-            mvwaddch(game, fruitYX[0], fruitYX[1], '@');
-            wrefresh(game);
-            wrefresh(score);
-        }
-
-        for (int i = 1; i < snakeLenght; i++) {
-            if (snakeYX[0][0] == snakeYX[0][i] && snakeYX[1][0] == snakeYX[1][i]) {
-                gameOver = 1;
-                playerScore = 0;
-                break;
-            }
-                
-        }
-
-        usleep(difficulty); //make the loop wait for some time
-        checkBor();
-        box (game, 0, 0); // fixing the borders
-
-    }
-        delwin(game);
-        delwin(score);
 
 }
 
@@ -173,9 +51,9 @@ void mainMenu(void) {
         }
         wrefresh(mainMenu);
 
-        input = wgetch(mainMenu); //getting the key that was pressed
+        menuInput = wgetch(mainMenu); //getting the key that was pressed
 
-        switch (input) {
+        switch (menuInput) {
             case (KEY_UP):
                 highlight--;
                 if (highlight == -1)
@@ -192,7 +70,7 @@ void mainMenu(void) {
                 switch (highlight) {
                     case (0):
                         delwin(mainMenu);
-                        play();
+                        play(difficulty, height, width, startY, startX);
                         goto mainMenuInt;
                         break;
                         
@@ -213,7 +91,6 @@ void mainMenu(void) {
     }
 
 }
-
 
 int main () {
 
